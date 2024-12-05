7f8869fe0ba46714de270d84dcb1528563a352b3
KYSTIK473 <73401296+KYSTIK473@users.noreply.github.com>
2024-12-05 14:34:01 +0300

    Itog

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 import math
 import matplotlib.pyplot as plt
 
-
 f = open('data.txt').readlines()
 hh = []
 for i in f:
@@ -9,22 +8,19 @@ for i in f:
     hh.append(h)
     if h > 60000:
         break
-print(hh)
 
 g = 9.81
 theta_0 = 90
-tau = 900
-t_end = 420
+tau = 87
+t_end = 360
 dt = 1
-
 
 
 def F(t):
     if t <= 101:
         return 6075 * 1000
     else:
-        return 2425 * 1000
-
+        return 2025 * 1000
 
 
 def theta(t, h=-1):
@@ -33,17 +29,15 @@ def theta(t, h=-1):
     return math.radians(theta_0 * math.exp(-t / tau))
 
 
-
 def m(t):
-    m0 = 403000  # начальная масса ракеты, кг
+    m0 = 403000
+    m1 = 155000
     if t <= 100:
-        return m0 - 1.96 * t
+        return m0 - 1960 * t
     else:
-        return m(100) - 0.653 * (t - 100)
+        return m1 - 653 * (t - 100)
 
 
-
-# Начальные условия
 h = 0
 v_y = 0
 v_x = 0
@@ -51,25 +45,23 @@ x = 0
 
 # Симуляция
 heights = [h]
-xs = [x]
 for t in range(0, t_end, dt):
     thrust = F(t)
     angle = theta(t, h)
     mass = m(t)
     a_y = (thrust * math.sin(angle) - mass * g) / mass
-    a_x = (thrust * math.cos(angle)) / mass
-
     v_y += a_y * dt
-    v_x += a_x * dt
     h += v_y * dt
-    x += v_x * dt
 
     heights.append(h)
-    xs.append(x)
-    print(t, thrust, math.degrees(angle), mass, h, v_y)
     if h > 60000:
         break
-print("Высота ракеты с течением времени:", heights)
+
+while len(heights) < len(hh):
+    heights.append(heights[-1])
+
+print(sum([abs(heights[i] - hh[i]) / hh[i] for i in range(len(hh))]) / len(hh))
 plt.plot(heights)
 plt.plot(hh)
+plt.legend(('Моделирование', 'KSP сиимуляция'))
 plt.show()
